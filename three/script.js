b655e60bb1bcf5f3ed1cbb54cda0ac1269f95685
digidoor <alan.busovaca@asu.edu>
2023-03-02 20:50:59 -0800

    added style.css

--- three/script.js
+++ three/script.js
@@ -1,1 +1,15 @@
-console.log("This is a test.");
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword()
+{
+	var password = generatePassword();
+	var passwordText = document.querySelector("#password");
+
+	passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
